c1d431a4e41e82e96489af635b3b28a63098b614
Evgenia Morozova <72866082+Morevq@users.noreply.github.com>
2026-04-12 15:29:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+    # Добавьте свое имя и первую букву фамилии в этот список
+    students = [
+        "Иван И.",
+        "Анна С.",
+        "Петр В."
+    ]
+    
+    print("Участники команды:")
+    for student in students:
+        print(f"- {student}")
+
+if __name__ == "__main__":
+    main()
